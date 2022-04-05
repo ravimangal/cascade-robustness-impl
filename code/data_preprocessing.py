added architectures and data processing for MNIST and CIFAR; fixed calculation of underapproximating box

code/data_preprocessing.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -8,7 +9,43 @@ from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow_datasets as tfds
 
+from tensorflow.data import Dataset
+
+##############################################################
+# Safescad and Safescad2 related functions
+
+def prepare_inputs(X):
+    # scales inputs using "standard scaler", and returns 2D numpy array
+    scaler = StandardScaler().fit(X)
+    return scaler
+
+def prepare_target(y, categories):
+    # convert target to categorical, and returns 2D numpy array
+    y = y.to_numpy().reshape(-1,1)
+    onehot = OneHotEncoder(categories=categories)
+    onehot.fit(y)
+    return onehot
+
+def save_encoders(scaler, onehot, output_dir):
+    if scaler is not None:
+        pkl.dump(scaler, open(f'{output_dir}/scaler.pkl', 'wb'))
+    if onehot is not None:
+        pkl.dump(onehot, open(f'{output_dir}/onehot.pkl', 'wb'))
+
+def save_data(X_enc, y_enc, features, onehot, onehot_name, fname, data_dir='../experiments/data'):
+    labels = onehot.get_feature_names(input_features=[onehot_name])
+    df = pd.concat([pd.DataFrame(X_enc, columns=features),
+                          pd.DataFrame(y_enc, columns=labels)],
+                        axis=1).astype({k:int for k in labels})
+    data_csv = f'{data_dir}/{fname}.csv'
+    df.to_csv(data_csv)
+    print(f'wrote data to {data_csv}')
+
+def add_noise(noise):
+    f = lambda x: x + tf.random.normal(tf.shape(x), stddev=noise)
+    return f
 
 ##############################################################
 # SafeSCAD specific functions
@@ -244,41 +281,219 @@ def get_safescad2_data(dataset_file, data_dir, noise, splits):
         print('Feature Names', feature_names)
 
         return (X_train_enc.values, y_train_enc.values, X_test_enc.values, y_test_enc.values)
-##############################################################
 
 ##############################################################
-# Generic functions
-def prepare_inputs(X):
-    # scales inputs using "standard scaler", and returns 2D numpy array
-    scaler = StandardScaler().fit(X)
-    return scaler
+## MNIST and CIFAR related functions####
 
-def prepare_target(y, categories):
-    # convert target to categorical, and returns 2D numpy array
-    y = y.to_numpy().reshape(-1,1)
-    onehot = OneHotEncoder(categories=categories)
-    onehot.fit(y)
-    return onehot
+def data_augmentation(
+        flip=True,
+        saturation=(0.5, 1.2),
+        contrast=(0.8, 1.2),
+        zoom=0.1,
+        noise=None,
+        cutout=None,
+):
+    def augment(x, y):
+        batch_size = tf.shape(x)[0]
+        input_shape = x.shape[1:]
 
-def save_encoders(scaler, onehot, output_dir):
-    if scaler is not None:
-        pkl.dump(scaler, open(f'{output_dir}/scaler.pkl', 'wb'))
-    if onehot is not None:
-        pkl.dump(onehot, open(f'{output_dir}/onehot.pkl', 'wb'))
+        # Horizontal flips
+        if flip:
+            x = tf.image.random_flip_left_right(x)
 
-def save_data(X_enc, y_enc, features, onehot, onehot_name, fname, data_dir='../experiments/data'):
-    labels = onehot.get_feature_names(input_features=[onehot_name])
-    df = pd.concat([pd.DataFrame(X_enc, columns=features),
-                          pd.DataFrame(y_enc, columns=labels)],
-                        axis=1).astype({k:int for k in labels})
-    data_csv = f'{data_dir}/{fname}.csv'
-    df.to_csv(data_csv)
-    print(f'wrote data to {data_csv}')
+        # Randomly adjust the saturation and contrast.
+        if saturation is not None and input_shape[-1] == 3:
+            x = tf.image.random_saturation(
+                x, lower=saturation[0], upper=saturation[1])
 
-def add_noise(noise):
-    f = lambda x: x + tf.random.normal(tf.shape(x), stddev=noise)
-    return f
+        if contrast is not None:
+            x = tf.image.random_contrast(
+                x, lower=contrast[0], upper=contrast[1])
 
+        # Randomly zoom.
+        if zoom is not None:
+            widths = tf.random.uniform([batch_size], 1. - zoom, 1.)
+            top_corners = tf.random.uniform(
+                [batch_size, 2], 0, 1. - widths[:, None])
+            bottom_corners = top_corners + widths[:, None]
+            boxes = tf.concat((top_corners, bottom_corners), axis=1)
+
+            x = tf.image.crop_and_resize(
+                x, boxes,
+                box_indices=tf.range(batch_size),
+                crop_size=input_shape[0:2])
+
+        if noise is not None:
+            x = x + tf.random.normal(tf.shape(x), stddev=noise)
+
+        if cutout is not None:
+            x = cutout_augmentation(x, mask_size=cutout)
+
+        return x, y
+
+    return augment
+
+
+def sample_beta_distribution(n, alpha, beta):
+    gamma_1 = tf.random.gamma(shape=(n,), alpha=alpha)
+    gamma_2 = tf.random.gamma(shape=(n,), alpha=beta)
+
+    return gamma_1 / (gamma_1 + gamma_2)
+
+
+def mixup_augmentation(D_1, D_2, alpha=0.2):
+    X_1, y_1 = D_1
+    X_2, y_2 = D_2
+
+    batch_size = tf.shape(X_1)[0]
+    input_dims = (1 for _ in range(len(tf.shape(X_1[1:]))))
+
+    lam = sample_beta_distribution(batch_size, alpha, alpha)
+
+    # Enable broadcasting to shape of X.
+    lam_x = tf.reshape(lam, (batch_size, *input_dims))
+
+    return (
+        X_1 * lam_x + X_2 * (1. - lam_x),
+        y_1 * lam[:, None] + y_2 * (1. - lam[:, None]))
+
+
+def cutout_augmentation(x, mask_size=8):
+    nhwc = tf.shape(x)
+    N = nhwc[0]
+    h = nhwc[1]
+    w = nhwc[2]
+
+    mask_corners_y = tf.random.uniform(
+        (N,), 0, h - mask_size + 1, dtype='int32')[:, None, None, None]
+    mask_corners_x = tf.random.uniform(
+        (N,), 0, w - mask_size + 1, dtype='int32')[:, None, None, None]
+
+    inds = tf.reshape(tf.range(h * w), (h, w))[None, :, :, None]
+
+    mask = tf.cast(
+        tf.logical_and(
+            tf.logical_and(
+                mask_corners_x <= inds % w,
+                inds % w < mask_corners_x + mask_size),
+            tf.logical_and(
+                mask_corners_y <= inds // w,
+                inds // w < mask_corners_y + mask_size)),
+        'float32')
+
+    return (1. - mask) * x + mask * 0.5
+
+
+def get_data(dataset, batch_size, augmentation=None, mixup=False):
+    # Get the augmentation.
+    if augmentation is None or augmentation.lower() == 'none':
+        augmentation = data_augmentation(
+            flip=False, saturation=None, contrast=None, zoom=None)
+
+    elif augmentation == 'all':
+        augmentation = data_augmentation()
+
+    elif augmentation == 'standard':
+        augmentation = data_augmentation(
+            saturation=None, contrast=None, zoom=0.25)
+
+    elif augmentation == 'standard_cutout':
+        augmentation = data_augmentation(
+            saturation=None, contrast=None, zoom=0.25, cutout=8)
+
+    elif augmentation == 'no_flip':
+        augmentation = data_augmentation(
+            flip=False, saturation=None, contrast=None, zoom=0.25)
+
+    elif augmentation.startswith('rs'):
+        flip = data_augmentation.startswith('rs_no_flip')
+
+        if augmentation.startswith('rs.'):
+            noise = float(augmentation.split('rs.')[1])
+
+        elif augmentation.startswith('rs_no_flip.'):
+            noise = float(augmentation.split('rs_no_flip.')[1])
+
+        else:
+            noise = 0.125
+
+        augmentation = data_augmentation(
+            flip=flip, saturation=None, contrast=None, noise=noise)
+
+    else:
+        raise ValueError(f'unknown augmentation type: {augmentation}')
+
+    # Load the data.
+
+    tfds_dir = os.environ['TFDS_DIR'] if 'TFDS_DIR' in os.environ else None
+
+    split = ['train', 'test']
+
+    (train, test), metadata = tfds.load(
+        dataset,
+        data_dir=tfds_dir,
+        split=split,
+        with_info=True,
+        shuffle_files=True,
+        as_supervised=True)
+
+    train = (train
+             .map(
+        lambda x, y: (tf.cast(x, 'float32') / 255., y),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        deterministic=False)
+             .cache()
+             .batch(batch_size)
+             .map(
+        augmentation,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        deterministic=False)
+             .prefetch(tf.data.experimental.AUTOTUNE))
+
+    test = (test
+            .map(
+        lambda x, y: (tf.cast(x, 'float32') / 255., y),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        deterministic=False)
+            .cache()
+            .batch(batch_size)
+            .prefetch(tf.data.experimental.AUTOTUNE))
+
+    if mixup:
+        train_2, _, _ = get_data(
+            dataset, batch_size, augmentation=augmentation, mixup=False)
+
+        train = Dataset.zip((train, train_2)).map(
+            mixup_augmentation,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+            deterministic=False)
+
+    num_classes = metadata.features['label'].num_classes
+
+    traindata_u = train.unbatch()
+    X_train = np.stack([x for x, y in traindata_u], axis=0)
+    y_train = np.stack([y for x, y in traindata_u], axis=0)
+    y_train = np.squeeze(np.eye(num_classes)[y_train.reshape(-1)])
+
+
+
+    testdata_u = test.unbatch()
+    X_test = np.stack([x for x, y in testdata_u], axis=0)
+    y_test = np.stack([y for x, y in testdata_u], axis=0)
+    y_test = np.squeeze(np.eye(num_classes)[y_test.reshape(-1)])
+
+    return (X_train, y_train, X_test, y_test)
+
+def get_mnist_data(dataset_file, data_dir, noise, splits):
+    print('Getting MNIST dataset...')
+    return get_data('mnist', 128)
+##############################################################
+
+def get_cifar_data(dataset_file, data_dir, noise, splits):
+    print('Getting CIFAR dataset...')
+    return get_data('cifar', 128)
+
+##############################################################
 
 def get_data(experiment, dataset_file, data_dir, noise=None, splits=None):
     print("Loading and preprocessing data ...")

code/verify.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import signal
 import time
+import math
 from threading import Timer
 
 import tensorflow as tf
@@ -86,12 +87,17 @@ def print_marabou_query(x, y, epsilon, path, num_classes, isOver=False):
     for i in range(ord('a'), ord('a') + num_classes - 1):
         names.append(chr(i))
 
+    if isOver:
+        dist = epsilon
+    else:
+        dist = epsilon/math.sqrt(2)
+
     for i in range(0, len(incorrect_labels)):
         fpath = f'{path}{names[i]}.txt'
         with open(fpath, 'w') as f1:
             for j in range(np.shape(x)[0]):
-                x_lb = x[j] - epsilon
-                x_ub = x[j] + epsilon
+                x_lb = x[j] - dist
+                x_ub = x[j] + dist
                 f1.write(f'x{j} >= {x_lb}\n')
                 f1.write(f'x{j} <= {x_ub}\n')
             f1.write(f'+y{incorrect_labels[i]} -y{y} >= 0')